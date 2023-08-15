baf8b3ae0f6748fc7f2354e8ce0146b9259f21ff
Princewill Awili <princewill.awili@gmail.com>
2023-08-15 19:10:13 +0100

    Completed Rooftop Page

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -3,8 +3,6 @@ import img1 from '../../assets/lucecollectibles/luce1.jpg'
 import img2 from '../../assets/lucecollectibles/dish3.jpg'
 import img3 from "../../assets/lucecollectibles/guest1.jpg";
 
-
-import aimg1 from '../../assets/acquacollectibles/poolrelax.jpg'
 import aimg2 from '../../assets/acquacollectibles/pool5.jpg'
 import aimg3 from "../../assets/acquacollectibles/pool3.jpg";
 import aimg4 from "../../assets/acquacollectibles/poolrelax2.jpg";
@@ -14,12 +12,20 @@ import himg2 from "../../assets/hotelcollectibles/balcony.jpg";
 import himg3 from "../../assets/hotelcollectibles/bath.jpg";
 import himg4 from "../../assets/hotelcollectibles/room4.jpg";
 
+import rimg1 from "../../assets/roofcollectibles/rooftop.jpg"
+import rimg2 from "../../assets/roofcollectibles/roof5.webp"
+import rimg3 from "../../assets/roofcollectibles/roof4.jpg";
+import rimg4 from "../../assets/roofcollectibles/roofSleeper.jpg";
+import rimg5 from "../../assets/roofcollectibles/roof2.webp";
+import rimg6 from "../../assets/roofcollectibles/roof3.jpg";
 
 
 
 
 
-const Slider = ({luce,acqua,hotel}) => {
+
+
+const Slider = ({luce,acqua,hotel,roof}) => {
   return (
     <div className='slider'>
       {luce && (
@@ -84,6 +90,26 @@ const Slider = ({luce,acqua,hotel}) => {
             <img src={himg2} alt='slideImg' className='slideImg' />
             <img src={himg3} alt='slideImg' className='slideImg' />
             <img src={himg4} alt='slideImg' className='slideImg' />
+          </div>
+        </>
+      )}
+      {roof && (
+        <>
+          <div className='imgHolder'>
+            <img src={rimg1} alt='slideImg' className='slideImg' />
+            <img src={rimg2} alt='slideImg' className='slideImg' />
+            <img src={rimg3} alt='slideImg' className='slideImg' />
+            <img src={rimg4} alt='slideImg' className='slideImg' />
+            <img src={rimg5} alt='slideImg' className='slideImg' />
+            <img src={rimg6} alt='slideImg' className='slideImg' />
+          </div>
+          <div className='imgHolder'>
+            <img src={rimg1} alt='slideImg' className='slideImg' />
+            <img src={rimg2} alt='slideImg' className='slideImg' />
+            <img src={rimg3} alt='slideImg' className='slideImg' />
+            <img src={rimg4} alt='slideImg' className='slideImg' />
+            <img src={rimg5} alt='slideImg' className='slideImg' />
+            <img src={rimg6} alt='slideImg' className='slideImg' />
           </div>
         </>
       )}
--- src/pages/Rooftop/Rooftop.js
+++ src/pages/Rooftop/Rooftop.js
@@ -1,8 +1,14 @@
 import './rooftop.css'
+import Slider from '../../components/Slider/Slider'
+import TextContent from '../../components/TextContent/TextContent'
+import { roofTxt } from '../../static'
 
 const Rooftop = () => {
   return (
-    <div className='rooftop'>rooftop</div>
+    <div className='rooftop'>
+      <Slider roof/>
+      <TextContent header={'Encounters at Sunset'} body={roofTxt} links={['ASK FOR INFO','START THE EXERIENCE']} footer={'Rooftop'}/>
+    </div>
   )
 }
 
